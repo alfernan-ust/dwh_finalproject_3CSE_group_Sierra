85b90a1165d5b021afcfa07b704e27868aaaea7d
marshiemallo <marshie.repo@gmail.com>
2025-12-03 15:55:33 +0800

    added age for user,staff, and merchant

--- scripts/customer_management.py
+++ scripts/customer_management.py
@@ -1,5 +1,11 @@
 from dataframe import set_frame, append_files
 import glob
+import datetime
+from datetime import date
+
+def calculate_age(born):
+    today = date.today()
+    return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
 
 # Credit card
 credit_card_files = glob.glob('/dataset/CustomerManagementDepartment/user_credit_card*')
@@ -22,6 +28,12 @@ df = set_frame(user_data_files[0])
 if len(user_data_files) > 1:
     user_data_files.pop(0)
     df = append_files(df, user_data_files)
+
+age_list = []
+for index, row in df.iterrows():
+    birthdate_raw = row['birthdate']
+    birthdate = datetime.date(int(birthdate_raw[:4]), int(birthdate_raw[5:7]), int(birthdate_raw[8:10]))
+    age_list.append(calculate_age(birthdate))
 
 df.to_parquet('/dataset/user_data.parquet')
 
